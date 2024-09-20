0117ff826a63bd240c53f35ac16844fc63e4558c
EmmanuelKevinS <emmanuel.kevin@ventitechnologies.com>
2024-09-20 16:57:47 +0800

    hi

--- OOP/studentClass.cpp
+++ OOP/studentClass.cpp
@@ -6,6 +6,30 @@ class Student{
         int age;
         int rollNumber;
     public:
+        //Default Constructor
+        Student(){
+            age = 0;
+            rollNumber= 0;
+            cout << "Default Constructor called " << "Age: " << age << " " << "Rollnumber: " << rollNumber << endl;
+        }
+
+        //Parameterised constructor
+        Student(int age){
+            this->age = age;
+            rollNumber = 0;
+            cout << "Parameterised constructor 1 called"  << "Age: " << age << " " << "Rollnumber: " << rollNumber << endl;
+        }
+        Student(int age, int rollNumber){
+            this->age = age;
+            this->rollNumber = rollNumber;
+            cout << "Parameterised constructor 2 called"  << "Age: " << age << " " << "Rollnumber: " << rollNumber << endl;
+        }
+
+        void display(){
+            cout << "Hello im the display function" << endl;
+        }
+
+        //Getters and Setters
         int getAge(){
             return age;
         }
