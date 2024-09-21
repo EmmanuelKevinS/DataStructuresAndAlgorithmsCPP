4744fed1b6ac01f0f1d8ff874fda28753d0f1eb9
emmanuelkevins <emmanuelkevins24@gmail.com>
2024-09-21 21:45:49 +0800

    hello

--- OOP/fractionClass.cpp
+++ OOP/fractionClass.cpp
@@ -1,10 +1,124 @@
 #include<iostream>
+#include<iomanip>
+#include<algorithm>
 using namespace std;
 
 class Fraction{
     private:
+        int numerator;
+        int denominator;
 
+    public:
+        // getters and setters
+        int getNum(){
+            return numerator;
+        }
+        int getDem(){
+            return denominator;
+        }
+        void setNum(int numerator){
+            this->numerator = numerator;
+        }
+        void setDem(int denominator){
+            this->denominator = denominator;
+        }
 
+        //constructors
+        Fraction(){
+            numerator = 1;
+            denominator = 1;
+        }
 
+        Fraction(int numerator){
+            this->numerator = numerator;
+            denominator = 1;
+        }
+        Fraction(int numerator, int denominator){
+            this->numerator = numerator;
+            this->denominator = denominator;
+        }
+        Fraction(Fraction &other){
+            this->numerator = other.numerator;
+            this->denominator = other.denominator;
+        }
+
+        //functions
+        float calculateFraction(){
+            //calculating float to 1 dp
+            float value = static_cast<float>(numerator) / denominator;  
+            cout << fixed << setprecision(1);  
+            return value;          
+        }
+
+        void display(){
+            cout << this->numerator << "/" << this->denominator << " = " << calculateFraction() << endl;
+        }
+
+        // void add(Fraction frac) {
+        //     // Step 1: Calculate common denominator
+        //     int commonDenominator = this->denominator * frac.denominator;
+
+        //     // Step 2: Calculate the new numerator for both fractions
+        //     int newFirstNumerator = (this->numerator * frac.denominator);
+        //     int newSecondNumerator = (frac.numerator * this->denominator);
+            
+        //     // Step 3: Add the numerators together
+        //     int finalNumerator = newFirstNumerator + newSecondNumerator;
+
+        //     // Step 4: Set the new numerator and denominator
+        //     this->numerator = finalNumerator;
+        //     this->denominator = commonDenominator;
+
+        //     // Step 5: Simplify the resulting fraction
+        //     pair<int, int> result = simplify();
+
+        //     // Step 6: Output the result
+        //     cout << "-----------------" << endl;
+        //     cout << "First fraction: " << this->numerator << "/" << this->denominator << endl;
+        //     cout << "Second fraction: " << frac.numerator << "/" << frac.denominator << endl;
+        //     cout << "Fraction after addition: " << result.first << " / " << result.second << endl;
+        //     cout << "Decimal value = " << fixed << setprecision(1) << static_cast<float>(result.first) / result.second << endl;
+        //     cout << "-----------------" << endl;
+        // }
+
+        void add(Fraction frac){
+            int commonDenominator = this->denominator * frac.denominator;
+            int newFirstNumerator = (this->numerator * frac.denominator);
+            int newSecondNumerator = (this->denominator * frac.numerator);
+            int finalNumerator = newFirstNumerator + newSecondNumerator;
+
+            cout << "-----------------" << endl;
+            cout << "First fraction: " << this->numerator << "/" << this->denominator << endl;
+            cout << "Second fraction: " << frac.numerator << "/" << frac.denominator << endl;
+            cout << "Fraction: " << finalNumerator << " / " << commonDenominator << endl;
+
+            this->numerator = finalNumerator;
+            this->denominator = commonDenominator;
+
+            pair<int, int> result = simplify();
+            this->numerator = result.first;
+            this->denominator = result.second;
+            cout << "Fraction after simplification: " << result.first << " / " << result.second << endl;
+            cout << "Decimal value = " << fixed << setprecision(1) << static_cast<float>(result.first) / result.second << endl;
+            cout << "-----------------" << endl;
+        }
+
+        pair<int, int> simplify(){
+            //to determine Greatest Common Divisor of the numerator and denominator
+            int gcd = 1;
+            int smallerNumber = min(abs(this->numerator), abs(this->denominator));
+            for(int i = 1; i <= smallerNumber; i++){
+                if((this->numerator % i == 0) && (this->denominator % i == 0)){
+                    gcd = i;
+                } 
+            }
+            cout << "GCD: " << gcd << endl;
+            int simplifiedNumerator = this->numerator / gcd;
+            int simplifiedDenominator = this->denominator / gcd;
+            return make_pair(simplifiedNumerator, simplifiedDenominator);
+        }
+
+        //destructor
+        ~Fraction(){}
 
 };
--- OOP/fractionMain.cpp
+++ OOP/fractionMain.cpp
@@ -4,7 +4,30 @@ using namespace std;
 
 int main(){
 
+    Fraction f1;
+    Fraction f2(6);
+    Fraction *f3 = new Fraction(4, 2);
+    Fraction *f4 = new Fraction(40, 22);
+    Fraction *f5 = new Fraction(41, 24);
+    Fraction *f6 = new Fraction(14, 3);
+    Fraction *f7 = new Fraction(12, 6);
 
+    f1.display();
+    f2.display();
+    f3->display();
+    f4->display();
+    f5->display();
+    f6->display();
+    f7->display();
+
+    f4->add(*f6);
+    f5->add(*f3);
+
+    delete f3;
+    delete f4;
+    delete f5;
+    delete f6;  
+    delete f7;  
 
     return 0;
 }
