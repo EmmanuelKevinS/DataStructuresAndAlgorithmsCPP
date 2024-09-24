90c611b40b4cb57be635f79930ca0f74681fb001
EmmanuelKevinS <emmanuel.kevin@ventitechnologies.com>
2024-09-24 19:15:56 +0800

    jet

--- OOP/shallowAndDeepCopy.cpp
+++ OOP/shallowAndDeepCopy.cpp
@@ -17,9 +17,26 @@ class Student{
             this->name = new char[strlen(name) + 1];
             strcpy(this->name, name);
         }
+        //copy constructor
+        Student(Student const &stud){
+            this->age = stud.age;
+            this->name = new char[strlen(stud.name) + 1];
+            strcpy(this->name, stud.name);
+        }
         void display(){
             cout << "Name: " << name << ", Age: " << age << endl;
         }
+        //getter and setter
+        char* getName(){
+            return this->name;
+        }
+        void setName(char *name){
+            this->name = new char[strlen(name) + 1];
+            strcpy(this->name, name);
+        }
+
+        //destructor
+        ~Student(){}
 };
 
 int main(){
@@ -28,11 +45,17 @@ int main(){
     Student *s1 = new Student(20, name);
     s1->display();
 
-    name[3] = 'E';
-    Student *s2 = new Student(30, name);
-    s2->display();
-
+    Student s2(*s1);
+    s2.getName()[3] = 'E';
+    cout << "S1: ";
     s1->display();
+    cout << "S2: ";
+    s2.display();
+    // name[3] = 'E';
+    // Student *s2 = new Student(30, name);
+    // s2->display();
+
+    // s1->display();
 
     return 0;
 }
